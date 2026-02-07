e261040a5ec611da6dca82bfc1921fb45929ce65
SK Maqshood <skmaqshood99@gmail.com>
2026-02-07 19:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+	document.getElementById("button").addEventListner("click",function(event){
+	   event.preventDefault();
+		let name= document.getElementById("name").value;
+		let year= document.getElementById("year").value;
+
+		let url = `https://localhost:8080/?name=${name}&year=&{year}`;
+		document.getElementById("url").textContent=url;
+	});
